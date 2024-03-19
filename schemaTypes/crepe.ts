41ea245869c7cad7bdef43cf052dfd9d58a39782
Logan Williams <logan@fireflysoftware.dev>
2024-03-19 00:12:53 +0000

    Created schema crepe

---
+++ schemaTypes/crepe.ts
@@ -0,0 +1,48 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'crepe',
+  title: 'Crepe',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+        isUnique: (value, context) => context.defaultIsUnique(value, context),
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+        },
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      date: 'date',
+      media: 'image',
+    },
+  },
+})
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import Crepe from './crepe'
+
+export const schemaTypes = [Crepe]
